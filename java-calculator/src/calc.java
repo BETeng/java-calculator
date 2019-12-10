3490fce836b9f2fe424e6a24b65c60dafd876558
LG BeTeng <brian7.teng@gmail.com>
2019-12-10 14:52:55 -0600

    initial

---
+++ java-calculator/src/calc.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class calc {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a number");
+		System.out.print("Enter a number");
+
+	}
+
+}
