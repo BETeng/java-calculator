56eee5087b585b55196515ca8bc7126a6b4a7dc4
LG BeTeng <brian7.teng@gmail.com>
2019-12-10 17:54:30 -0600

    num1,2 and op

--- java-calculator/src/calc.java
+++ java-calculator/src/calc.java
@@ -2,10 +2,20 @@ import java.util.Scanner;
 public class calc {
 
 	public static void main(String[] args) {
+		
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter a number");
-		System.out.print("Enter a number");
-
+		
+		System.out.println("Enter a number");
+		double num1 = scan.nextDouble();
+		System.out.println(num1);
+		
+		System.out.println("Enter an operator");
+		String op = scan.next();
+		System.out.println(op);
+		
+		System.out.println("Enter a number");
+		double num2 = scan.nextDouble();
+		System.out.println(num2);
 	}
 
 }
